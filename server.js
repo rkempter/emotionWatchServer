f2661bc1698a0d30ed42d24931636d827eef2de2
rkempter <renato.kempter@gmail.com>
2012-11-14 00:31:53 +0100

    Handle query results with a recursive method -> use callbacks to send response

--- server.js
+++ server.js
@@ -38,12 +38,53 @@ Date.prototype.toMysqlFormat = function() {
     return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
 };
 
+function queryData(index, currentDate, step, emotions, callback) {
+  
+  if(index > 0) {
+    endDate = new Date(currentDate.getTime() + step * 1000);
+    var query = connection.query("SELECT AVG(wc.`love`) AS love , AVG(wc.`pride`) AS pride, AVG(wc.`surprise`) as surprise, AVG(wc.`excitement`) as excitement, AVG(wc.`joy`) as joy, AVG(wc.`like`) as liking, AVG(wc.`anger`) as anger, AVG(wc.`shame`) as shame, AVG(wc.`shock`) as shock, AVG(wc.`anxiety`) as anxiety, AVG(wc.`sadness`) as sadness, AVG(wc.`dislike`) as dislike  FROM weibo_category as wc, weibo_olympics as wo WHERE wc.id = wo.id AND wo.dateTime > ? AND wo.dateTime < ?", [currentDate.toMysqlFormat(), endDate.toMysqlFormat()]);
+
+    query
+      .on('error', function(err) {
+        console.log('Query error!');
+      })
+
+      .on('result', function(row) {
+        var element = new Array();
+        var sum = 0;
+        
+        for(var index in row) {
+          if(!isNaN(row[index])) {
+            sum += parseFloat(row[index]);
+          }
+        }
+
+        if(sum > 0) {
+          for(var index in row) {
+            if(!isNaN(row[index])) {
+              element.push( { "emotion": index, "value": parseFloat(row[index]) / sum} );
+            }
+          }
+          emotions.push(element); 
+        }
+      })
+      .on('end', function() {
+        index--;
+        console.log(index);
+        queryData(index, endDate, step, emotions, callback);
+      })
+  } else {
+    console.log("Callback calling with "+emotions);
+    callback(emotions);
+  }
+}
+
 function getEmotionTweets(req, res, next) {
   // Resitify currently has a bug which doesn't allow you to set default headers
   // This headers comply with CORS and allow us to server our response to any origin
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  
+
   console.log(req.params);
   
   var windowSize = parseInt(req.params.windowSize);
@@ -55,33 +96,10 @@ function getEmotionTweets(req, res, next) {
   var startDate = new Date(req.params.currentDateTime);
   var currentDate = startDate;
   var endDate;
-
-  while(windowSize > 0) {
-    endDate = new Date(currentDate.getTime() + step * 1000);
-
-    var query = connection.query("SELECT AVG(wc.`love`) AS love , AVG(wc.`pride`) AS pride, AVG(wc.`surprise`) as surprise, AVG(wc.`excitement`) as excitement, AVG(wc.`joy`) as joy, AVG(wc.`like`) as liking, AVG(wc.`anger`) as anger, AVG(wc.`shame`) as shame, AVG(wc.`shock`) as shock, AVG(wc.`anxiety`) as anxiety, AVG(wc.`sadness`) as sadness, AVG(wc.`dislike`) as dislike  FROM weibo_category as wc, weibo_olympics as wo WHERE wc.id = wo.id AND wo.dateTime > ? AND wo.dateTime < ?", [currentDate.toMysqlFormat(), endDate.toMysqlFormat()]);
-
-    query
-      .on('error', function(err) {
-        console.log('Query error!');
-      })
-
-      .on('fields', function(fields) {
-        //console.log(fields);
-      })
-
-      .on('result', function(row) {
-        var element = new Array();
-        for(var index in row) {
-          element.push( { "emotion": index, "value": parseFloat(row[index]) / 30} );
-        }
-        emotions.push(element);
-      })
-
-    currentDate = endDate;
-    windowSize--;
-  }
-  res.send(emotions);
+  
+  queryData(windowSize, startDate, step, emotions, function(emotions) {
+    res.send(emotions);
+  }); 
 }
 
 server.get('/emotionTweets', getEmotionTweets);
