19b224d6c0bebbe35b5f968bccf3d466a7503aba
rkempter <renato.kempter@gmail.com>
2012-11-24 15:11:21 +0100

    Added median functionalities

--- queryTopic.js
+++ queryTopic.js
@@ -31,7 +31,6 @@ Connection.prototype.handleRow = function(row, previousData) {
         previousData[index] += parseFloat(row[index]);
       }
     }
-
     return previousData;
 }
 
@@ -48,11 +47,6 @@ Connection.prototype.mapValue = function(value, median, max) {
     } else {
       return value / median;
     }
-}
-
-
-Connection.prototype.setConnection =  function(connection) {
-    module.connection = connection;
 }
 
 /**
@@ -116,4 +110,89 @@ Connection.prototype.computeSpeed = function() {
 
 Connection.prototype.computeInternalMedian = function() {
 
+};
+
+Connection.prototype.getMedian = function(index) {
+    if(index == 0) {
+        this.medians = new Array();
+    }
+
+    if(index < this.categories.length) {
+        var category = this.categories[index];
+        var medianPosition = 0;
+        // get median position
+        var positionQuery = this.connection.query("SELECT COUNT(*) AS total FROM "+this.emotionTable+" as wc WHERE wc.`"+index+"` != 0");
+
+        positionQuery.on('error', function(err) {
+            console.log("Error happened in getMedian: "+err);
+        });
+
+        positionQuery.on('result', function(results) {
+            medianPosition = Math.floor(results[0]['total'] / 2);
+        });
+
+        positionQuery.on('end', function() {
+            this.queryMedian(index, medianPosition);
+        });
+    } else {
+        return;
+    }
+};
+
+Connection.prototype.queryMedian = function(index, position) {
+    var category = this.categories[index];
+    var medianQuery = connection.query("SELECT wc.`"+category+"` as `"+category+"` FROM "+this.emotionTable+" as wc WHERE wc.`"+category+"` != 0 ORDER BY wc.`"+this.category+"` ASC LIMIT ?, 1", [position]);
+
+    medianQuery.on('result', function(row) {
+        this.medians[category] = row[category];
+    });
+    
+    medianQuery.on('end', function() {
+        index++;
+        this.getMedian(index);
+    });
+}
+
+/**
+ * Creates an array with all categories from the category table
+ * Does not include the column names "id", "neutral", "contextual";
+ */
+Connection.prototype.getEmotionCategories = function(callback) {
+    var query = this.connection.query("SELECT * FROM "+this.emotionTable+" LIMIT 1");
+
+    query.on('error', function(err) {
+        console.log("Error happend in getEmotionCategories: "+err);
+    });
+
+    query.on('fields', function(fields) {
+        this.categories = new Array();
+        for(var index in fields) {
+            if(fields[index] != "id" && fields[index] != "neutral" && fields[index] != "contextual") {
+                this.categories.push(fields[index]);
+            }
+        }
+    });
+
+    callback();
+}
+
+/**
+ * Queries the maximal values in the emotion table
+ */
+Connection.prototype.getTableMax = function(callback) {
+    var maxQuery = this.connection.query("SELECT MAX(wc.`love`) AS love , MAX(wc.`pride`) AS pride, MAX(wc.`surprise`) as surprise, MAX(wc.`excitement`) as excitement, MAX(wc.`joy`) as joy, MAX(wc.`like`) as `like`, MAX(wc.`anger`) as anger, MAX(wc.`shame`) as shame, MAX(wc.`shock`) as shock, MAX(wc.`anxiety`) as anxiety, MAX(wc.`sadness`) as sadness, MAX(wc.`dislike`) as dislike FROM "+this.emotionTable+" as wc", function(err, rows) {
+      if(err) throw err;
+
+      this.maxValues = new Array();
+      var row = rows[0];
+
+      for(var index in row) {
+        if(!isNaN(row[index])) {
+          var x = { "label": index, "values": { "max": row[index] } };
+          this.maxValues.push(x);
+        }
+      }
+
+      callback();
+    });
 };
