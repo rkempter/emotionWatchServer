545f24a5ded8c33c5e4251c5ed572d53a95bd393
rkempter <renato.kempter@gmail.com>
2012-11-30 15:16:30 +0100

    Server modifications

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var sanitize = require('validator').sanitize;
 var tableConnector = require('./lib/queryTopic');
+var eventConnector = require('./lib/queryEvents')
 
 // Create connection object
 var connection = mysql.createConnection({
@@ -113,7 +114,7 @@ function getEvents(req, res, next) {
     console.log("Requested Events");
 
     var response = new Array();
-    
+
     var datetime = req.params.datetime;
 
     events.getCurrentEvents(datetime, response, function(array) {
@@ -132,6 +133,33 @@ function getAthlete(req, res, next) {
     
 }
 
+function getPatternWatches(req, res, next) {
+    // Resitify currently has a bug which doesn't allow you to set default headers
+    // This headers comply with CORS and allow us to server our response to any origin
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+    console.log('Pattern watches requested');
+
+    var response = new Array();
+
+    var startDateTime = new Date(req.params.startDateTime);
+    var endDateTime = new Date(req.params.endDateTime);
+    var keyword = req.params.keyword;
+    var network = req.params.network;
+    var windowSize = req.params.windowSize;
+
+    if(network == 'weibo') {
+        weibo.queryData(24, startDateTime, windowSize, response, function(array) {
+            res.send(array);
+        });
+    } else {
+        twitter.queryData(24, startDateTime, windowSize, response, function(array) {
+            res.send(array);
+        });
+    }
+}
+
 var restify = require('restify');
 var server = restify.createServer();
 server.use( restify.queryParser() );
@@ -144,6 +172,8 @@ server.get('/tweets', getTweets);
 server.get('/events', getEvents);
 
 server.get('/athlete', getAthlete);
+
+server.get('/getPatternWatches', getPatternWatches)
 
 server.listen(8080, function() {
   console.log('%s listening at %s, love & peace', server.name, server.url);
