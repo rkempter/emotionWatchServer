b255e187b69f86f90abf21df241e4eefab14503d
rkempter <renato.kempter@gmail.com>
2013-01-14 02:25:12 +0100

    push

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var twitter = tableConnector.createConnection({
 });
 
 var frontPage = startConnector.createConnection({
-    table: 'hashtag_profil',
+    table: 'hashtag_profile',
     connection: connection
 });
 
@@ -268,8 +268,10 @@ function getEventInfo(req, res, next) {
 
     var response = new Array();
 
-    events.getEventInfo(sport, startDateTime, endDateTime, response, function(array) {
-        res.send(array);
+    sports.queryHashtag(startDateTime, endDateTime, 'twitter', sport, undefined, response, function(startDateTime, endDateTime, sport, emotion, response) {
+        events.getEventInfo(startDateTime, endDateTime, sport.slice(1), response, function(array) {
+            res.send(array);
+        });
     });
 }
 
@@ -297,8 +299,10 @@ function getEventVideo(req, res, next) {
 
     var response = new Array();
 
-    events.getEventVideo(sport, startDateTime, endDateTime, response, function(array) {
-        res.send(array);
+    sports.queryHashtag(startDateTime, endDateTime, 'twitter', sport, undefined, response, function(startDateTime, endDateTime, sport, emotion, response) {
+        events.getEventVideo(sport.slice(1), startDateTime, endDateTime, response, function(array) {
+            res.send(array);
+        });
     });
 }
 
